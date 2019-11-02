66fc0456ee02a3ffd47f3f0cef91a4402b5a161d
det <det@iis.fhg.de>
2019-11-02 02:51:57 +0100

    added a fm demodulator app.

---
+++ experimental_code/demod/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+CC=gcc
+$CC -O3 -o fmdemod.app fmdemod.c -lm
---
+++ experimental_code/demod/fmdemod.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc,char** argv)
+{
+	FILE *f;
+	FILE *g;
+
+	signed short ipart,qpart;
+	double angle_old;
+	double angle_new;
+	double delta;
+	signed short pcm;
+
+	angle_old=angle_new=0;
+	if (argc!=3)
+	{
+		fprintf(stderr,"please run with %s INPUT.iq192 OUTPUT.pcm\n",argv[0]);
+		return 0;
+	}
+
+	f=fopen(argv[1],"rb");
+	g=fopen(argv[2],"wb");
+
+	fread(&ipart,sizeof(short),1,f);
+	while (!feof(f))
+	{
+		fread(&qpart,sizeof(short),1,f);
+		angle_old=angle_new;
+		angle_new=atan2(qpart,ipart);
+		delta=angle_new-angle_old;
+		if (delta<M_PI) delta+=2*M_PI;
+		if (delta>M_PI) delta-=2*M_PI;
+		pcm=(signed short)(delta*8192.0/M_PI);
+		fwrite(&pcm,sizeof(short),1,g);
+
+		fread(&ipart,sizeof(short),1,f);
+	}
+
+	fclose(f);
+	fclose(g);
+}
